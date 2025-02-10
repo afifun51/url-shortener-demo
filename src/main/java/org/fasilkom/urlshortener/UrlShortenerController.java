fe9ef64dc299e3c164b4378cf7e8d3087b5df3aa
afifun <afifun.naily@traveloka.com>
2025-02-11 03:40:48 +0700

    move controllers

---
+++ src/main/java/org/fasilkom/urlshortener/UrlShortenerController.java
@@ -0,0 +1,56 @@
+package org.fasilkom.urlshortener;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+
+@CrossOrigin(maxAge = 3600)
+@RestController
+@RequestMapping("/api")
+class UrlShortenerController {
+  private final Map<String, String> urlStorage = new HashMap<>();
+
+  @PostMapping("/shorten")
+  public Map<String, String> shortenUrl(@RequestParam String longUrl) {
+    String shortUrl = UUID.randomUUID().toString().substring(0, 8);
+    urlStorage.put(shortUrl, longUrl);
+    return Map.of("shortUrl", shortUrl);
+  }
+
+  @PostMapping("/shorten-custom")
+  public Map<String, String> shortenUrlCustom(@RequestParam String longUrl, @RequestParam String custom) {
+    urlStorage.put(custom, longUrl);
+    return Map.of("shortUrl", custom);
+  }
+
+  @PostMapping("/urls")
+  public Map<String, String> urls() {
+    return urlStorage;
+  }
+
+  @GetMapping("/{shortUrl}")
+  public Map<String, String> getOriginalUrl(@PathVariable String shortUrl) {
+    String longUrl = urlStorage.get(shortUrl);
+    if (longUrl == null) {
+      throw new UrlNotFoundException("Short URL not found");
+    }
+    return Map.of("longUrl", longUrl);
+  }
+}
+
+@ResponseStatus(code = org.springframework.http.HttpStatus.NOT_FOUND)
+class UrlNotFoundException extends RuntimeException {
+  public UrlNotFoundException(String message) {
+    super(message);
+  }
+}
