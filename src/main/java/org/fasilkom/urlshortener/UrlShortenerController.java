53e4d1654ac6a631b56677052b7bf36d9f809714
afifun <afifun.naily@traveloka.com>
2025-02-11 11:29:28 +0700

    add custom url

--- src/main/java/org/fasilkom/urlshortener/UrlShortenerController.java
+++ src/main/java/org/fasilkom/urlshortener/UrlShortenerController.java
@@ -26,6 +26,12 @@ class UrlShortenerController {
     return Map.of("shortUrl", shortUrl);
   }
 
+  @PostMapping("/shorten-custom")
+  public Map<String, String> shortenUrlCustom(@RequestParam String longUrl, @RequestParam String custom) {
+    urlStorage.put(custom, longUrl);
+    return Map.of("shortUrl", custom);
+  }
+
   @PostMapping("/urls")
   public Map<String, String> urls() {
     return urlStorage;
