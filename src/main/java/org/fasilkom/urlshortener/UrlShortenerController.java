43ce85d01bfa0b70ad3ba735f9bb4240a4dc5020
Moh Afifun Naily <afifun.naily@traveloka.com>
2025-02-12 14:21:21 +0700

    feature: add services (#10)

--- src/main/java/org/fasilkom/urlshortener/UrlShortenerController.java
+++ src/main/java/org/fasilkom/urlshortener/UrlShortenerController.java
@@ -1,17 +1,27 @@
 package org.fasilkom.urlshortener;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 
 @RestController
 @RequestMapping("/api")
 class UrlShortenerController {
   private final Map<String, String> urlStorage = new HashMap<>();
+
+  @PostMapping("/shorten")
+  public Map<String, String> shortenUrl(@RequestParam String longUrl) {
+    String shortUrl = UUID.randomUUID().toString().substring(0, 8);
+    urlStorage.put(shortUrl, longUrl);
+    return Map.of("shortUrl", shortUrl);
+  }
 }
 
 @ResponseStatus(code = org.springframework.http.HttpStatus.NOT_FOUND)
