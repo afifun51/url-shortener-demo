976914928a287d41208a4b668e4cd917ce48a35b
afifun <afifun.naily@traveloka.com>
2025-02-12 11:10:08 +0700

    add lines

--- src/main/java/org/fasilkom/urlshortener/UrlShortenerController.java
+++ src/main/java/org/fasilkom/urlshortener/UrlShortenerController.java
@@ -1,8 +1,11 @@
 package org.fasilkom.urlshortener;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -20,5 +23,32 @@ class UrlShortenerController {
     String shortUrl = UUID.randomUUID().toString().substring(0, 8);
     urlStorage.put(shortUrl, longUrl);
     return Map.of("shortUrl", shortUrl);
+  }
+
+  @PostMapping("/shorten-custom")
+  public Map<String, String> shortenUrlCustom(@RequestParam String longUrl, @RequestParam String custom) {
+    urlStorage.put(custom, longUrl);
+    return Map.of("shortUrl", custom);
+  }
+
+  @PostMapping("/urls")
+  public Map<String, String> urls() {
+    return urlStorage;
+  }
+
+  @GetMapping("/{shortUrl}")
+  public Map<String, String> getOriginalUrl(@PathVariable String shortUrl) {
+    String longUrl = urlStorage.get(shortUrl);
+    if (longUrl == null) {
+      throw new UrlNotFoundException("Short URL not found");
+    }
+    return Map.of("longUrl", longUrl);
+  }
+}
+
+@ResponseStatus(code = org.springframework.http.HttpStatus.NOT_FOUND)
+class UrlNotFoundException extends RuntimeException {
+  public UrlNotFoundException(String message) {
+    super(message);
   }
 }
