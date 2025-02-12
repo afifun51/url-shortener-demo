987c340d363193a874fb6116d3ff310c86ee3088
afifun <afifun.naily@traveloka.com>
2025-02-12 10:56:03 +0700

    remove codesmell

--- src/main/java/org/fasilkom/urlshortener/UrlShortenerController.java
+++ src/main/java/org/fasilkom/urlshortener/UrlShortenerController.java
@@ -1,17 +1,11 @@
 package org.fasilkom.urlshortener;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 
 @RestController
